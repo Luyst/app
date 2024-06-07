9abc7f4e7c553ada7524008c91cfec23942a70a4
Luyst <dhuy15072003@gmail.com>
2024-06-07 18:48:13 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,75 @@
+from flask import Flask, render_template, request
+from flask_cors import CORS
+import joblib
+import numpy as np
+
+app = Flask(__name__)
+CORS(app)
+
+# Load scaler
+scaler = joblib.load('models/scaler.joblib')
+
+# Load KMeans model
+kmeans_model = joblib.load('models/model.joblib')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        # Lấy dữ liệu từ form
+        Education = (request.form['Education'])
+        Marital_Status = (request.form['Marital_Status'])
+        Income = float(request.form['Income'])
+        Wines = float(request.form['Wines'])
+        Fruits = float(request.form['Fruits'])
+        Meat = float(request.form['Meat'])
+        Fish = float(request.form['Fish'])
+        Sweet = float(request.form['Sweet'])
+        Gold = float(request.form['Gold'])
+        Age = int(request.form['Age'])
+        Children = int(request.form['Children'])
+        Expenses = int(request.form['Expenses'])
+
+        #Chuyển dữ liệu về dạng chuẩn
+        education_map = {
+            'Graduation': 1,
+            'PhD': 2,
+            'Master': 3,
+            'Basic': 0
+        }
+        marial_status_map = {
+            'In relationship': 1, 
+            'Single': 0
+        }
+        Education = int(education_map.get(Education))
+        Marital_Status = int(marial_status_map.get(Marital_Status))
+        # Tạo DataFrame từ dữ liệu nhập vào
+        data = np.array([[Education, Marital_Status, Income, Wines, Fruits, Meat, Fish, Sweet, Gold, Age, Children, Expenses]])
+
+        # Chuẩn hóa dữ liệu
+        scaled_data = scaler.transform(data)
+
+        # Dự đoán nhóm phân cụm cho dữ li   ệu
+        cluster = kmeans_model.predict(scaled_data)
+
+        # Đề xuất sản phẩm dựa trên cụm
+        recommendations = {
+            0: 'Wines, Fish, Gold',
+            1: 'Wines, Fish, Gold',
+            2: 'Wines, Fish, Gold',
+            3: 'Wines, Fish, Gold',
+        }
+
+        recommendation = recommendations.get(cluster[0], 'Không có đề xuất')
+
+        return render_template('index.html', cluster=cluster[0], recommendation=recommendation,info=data)
+
+    except Exception as e:
+        return str(e)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5500)
---
+++ models/create_model.py
@@ -0,0 +1,104 @@
+import numpy as np
+import pandas as pd
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import MinMaxScaler
+import joblib
+import warnings
+warnings.filterwarnings('ignore')
+
+# Load Data
+df = pd.read_csv('models/marketing_campaign.csv', delimiter='\t')
+df = df.dropna()
+
+df['Age'] = 2021 - df['Year_Birth']
+
+# Add new columns
+df['Kids'] = df['Kidhome'] + df['Teenhome']
+df['Expenses'] = df['MntWines'] + df['MntFruits'] + df['MntMeatProducts'] + df['MntFishProducts'] + df['MntSweetProducts'] + df['MntGoldProds']
+
+# Replace marital status values
+df['Marital_Status'] = df['Marital_Status'].replace({
+    'Married': 'In relationship', 'Together': 'In relationship',
+    'Divorced': 'Single', 'Widow': 'Single', 'Absurd': 'Single',
+    'Alone': 'Single', 'YOLO': 'Single'
+})
+
+# Label encode marital status
+df['Marital_Status'] = df['Marital_Status'].map({'In relationship': 1, 'Single': 0})
+
+# Replace education values
+df['Education'] = df['Education'].replace('2n Cycle', 'Master')
+
+# Label encode education
+education_mapping = {
+    'Graduation': 1,
+    'PhD': 2,
+    'Master': 3,
+    'Basic': 0
+}
+df['Education'] = df['Education'].map(education_mapping)
+
+# Drop unused columns
+df = df.drop(columns=[
+    'ID', 'Year_Birth', 'Dt_Customer', 'Kidhome', 'Teenhome', 'Recency', 
+    'NumDealsPurchases', 'NumWebPurchases', 'NumCatalogPurchases', 
+    'NumStorePurchases', 'NumWebVisitsMonth', 'AcceptedCmp1', 'AcceptedCmp2', 
+    'AcceptedCmp3', 'AcceptedCmp4', 'AcceptedCmp5', 'Complain', 'Response', 
+    'Z_CostContact', 'Z_Revenue'
+])
+
+# Rename columns
+df.columns = ['Education', 'Marital_Status', 'Income', 'Wines', 'Fruits', 'Meat', 'Fish', 'Sweet', 'Gold', 'Age', 'Children', 'Expenses']
+
+# Create a DataFrame subset for outlier detection
+df_subset = df[['Income', 'Age']]
+
+# Calculate z-scores
+from scipy import stats
+z_scores = np.abs(stats.zscore(df_subset))
+
+# Filter entries with z-scores within [-3, 3]
+filtered_entries = (z_scores < 3).all(axis=1)
+
+# Create a new DataFrame with filtered entries
+df1 = df[filtered_entries]
+df1.reset_index(inplace=True, drop=True)
+
+# Create a scaler and scale the data
+scaler = MinMaxScaler()
+df1_scaled = scaler.fit_transform(df1)
+df1_scaled = pd.DataFrame(df1_scaled, columns=df1.columns)
+
+# KMeans Clustering with k=4
+kmeans = KMeans(n_clusters=4, random_state=42)
+kmeans.fit(df1_scaled)
+
+# Dự đoán cụm cho từng điểm dữ liệu
+clusters = kmeans.predict(df1_scaled)
+
+# Thêm cột cluster vào DataFrame df1
+df1['Cluster'] = clusters
+
+# Phân tích đặc trưng của các cụm
+cluster_features = df1.groupby('Cluster').mean()
+
+# In ra thông tin về đặc trưng của các cụm
+print(cluster_features)
+
+for cluster_id, cluster_data in cluster_features.iterrows():
+    print(f"Cluster: {cluster_id}")
+    top_products = cluster_data[['Wines', 'Fruits', 'Meat', 'Fish', 'Sweet', 'Gold']].nlargest(3)
+    print(f"Top 3 Products: {top_products.index.tolist()}\n")
+
+
+# # Save the model and scaler
+joblib.dump(kmeans, 'kmeans_model.joblib')
+joblib.dump(scaler, 'scaler.joblib')
+
+
+# Test mô hình
+sample = np.array([[1, 0, 580000, 250, 30, 50, 20, 10, 5, 45, 2, 365]])  # Ví dụ dữ liệu
+sample_scaled = scaler.transform(sample)
+cluster = kmeans.predict(sample_scaled)
+
+print("Cluster: ", cluster)
